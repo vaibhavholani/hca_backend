042d6fe6ab1751a48f85f7cf2e60e6ea49e9180b
Vaibhav Holani <vaibhavholani@gmail.con>
2021-06-16 00:17:09 -0400

    final

--- app.py
+++ app.py
@@ -10,41 +10,41 @@ from Legacy_Data import add_party, add_suppliers
 
 app = Flask(__name__)
 
-@app.route('/supplier_names_and_ids', methods=['GET'])
+@app.route('/hca/supplier_names_and_ids', methods=['GET'])
 def get_all_supplier_names():
     data = retrieve_indivijual.get_all_names_ids('supplier')
     json_data = json.dumps(data)
     return json_data
 
-@app.route('/party_names_and_ids', methods=['GET'])
+@app.route('/hca/party_names_and_ids', methods=['GET'])
 def get_all_party_names():
     data = retrieve_indivijual.get_all_names_ids('party')
     json_data = json.dumps(data)
     return json_data
 
-@app.route('/bank_names_and_ids', methods=['GET'])
+@app.route('/hca/bank_names_and_ids', methods=['GET'])
 def get_all_bank_names():
     data = retrieve_indivijual.get_all_names_ids('bank')
     json_data = json.dumps(data)
     return json_data
 
-@app.route('/credit/<int:supplier_id>/<int:party_id>', methods=['GET'])
+@app.route('/hca/credit/<int:supplier_id>/<int:party_id>', methods=['GET'])
 def get_credit(supplier_id: int, party_id: int):
     data = retrieve_credit.get_credit(supplier_id, party_id)
     json_data = json.dumps(data)
     return json_data
 
-@app.route('/pending_bills/<int:supplier_id>/<int:party_id>', methods=['GET'])
+@app.route('/hca/pending_bills/<int:supplier_id>/<int:party_id>', methods=['GET'])
 def get_pending_bills(supplier_id: int, party_id: int):
     data = retrieve_register_entry.get_pending_bill_numbers(supplier_id, party_id)
     json_data = json.dumps(data)
     return json_data
 
-@app.route('/add/register_entry/<int:bill>/<int:amount>/<string:supplier>/<string:party>/<string:date>', methods=['GET'])
+@app.route('/hca/add/register_entry/<int:bill>/<int:amount>/<string:supplier>/<string:party>/<string:date>', methods=['GET'])
 def add_register_entry(bill: int, amount: int, supplier: str, party: str,  date: str):
     return RegisterEntry.create(bill, amount, json.loads(supplier), json.loads(party), date)
 
-@app.route('/create_report', methods=['POST'])
+@app.route('/hca/create_report', methods=['POST'])
 def create_report():
     if request.method == "POST":
         data = request.json
@@ -60,18 +60,18 @@ def create_report():
         return response
     return {"status":"okay"}
 
-@app.route('/add/individual/<string:type>/<string:name>/<string:phone>/<string:address>')
+@app.route('/hca/add/individual/<string:type>/<string:name>/<string:phone>/<string:address>')
 def add_individual(type: str, name: str, phone:str, address:str):
     insert_individual.add_individual(type, name, address)
     return {"status":"okay"}
 
-@app.route('/add/memo_entry/<string:object>')
+@app.route('/hca/add/memo_entry/<string:object>')
 def add_memo_entry(object: str):
     object = json.loads(object)
     print(object)
     return MemoEntry.call(object)
 
-@app.route('/add_legacy')
+@app.route('/hca/add_legacy')
 def add_legacy():
     add_suppliers.add()
     add_party.add()
