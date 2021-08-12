31c7afddffc1aef539d30c8d5308fcddc6f0ad4c
Vaibhav Holani <vaibhavholani@gmail.con>
2021-08-12 18:57:14 -0400

    minor bug fix

--- Reports/khata_report.py
+++ Reports/khata_report.py
@@ -75,14 +75,14 @@ def execute(party_ids: List[int], supplier_ids: List[int], start_date: str, end_
     """
     data = khata_report(party_ids, supplier_ids, start_date, end_date)
     
-    #### ONLINE VERSIONNNN
-    # io_file, pdf = show_pdf.show_pdf(data)
+    ### ONLINE VERSIONNNN
+    io_file, pdf = show_pdf.show_pdf(data)
 
-    # return (io_file, "khata_report.pdf")
+    return (io_file, "khata_report.pdf")
 
 
     #### OFFLINE VERSIONNNN
 
-    show_pdf.show_pdf(data)
+    # show_pdf.show_pdf(data)
 
 
