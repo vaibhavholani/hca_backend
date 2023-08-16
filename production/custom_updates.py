1b002ff37ca90ca20d511e5ef975189dac8896f0
Vaibhav Holani <vaibhavholani@gmail.con>
2023-08-15 20:42:16 -0400

    added code to repair broken data

--- production/custom_updates.py
+++ production/custom_updates.py
@@ -2,7 +2,28 @@ import os
 import sys
 sys.path.append("../")
 from psql import execute_query
+from Entities import MemoEntry
 
+def repair_data():
+    memo_entry_del_data = [
+    {"supplier_id": 2623, "party_id": 387, "memo_number": 884},
+    {"supplier_id": 2394, "party_id": 530, "memo_number": 2991},
+    {"supplier_id": 1685, "party_id": 621, "memo_number": 3147},
+    {"supplier_id": 2419, "party_id": 426, "memo_number": 3039},
+    {"supplier_id": 2623, "party_id": 387, "memo_number": 2964}]
+
+    # Assuming MemoEntry class has a retrieve method
+    for entry_data in memo_entry_del_data:
+        memo_entry = MemoEntry.retrieve(
+            supplier_id=entry_data["supplier_id"],
+            party_id=entry_data["party_id"],
+            memo_number=entry_data["memo_number"]
+        )
+        
+        if memo_entry:
+            # Assuming there's a delete method in MemoEntry class
+            memo_entry.delete()
+    
 
 def execute_sql_file(filename):
     with open(filename, 'r') as f:
@@ -21,6 +42,11 @@ if __name__ == "__main__":
 
     try: 
         execute_sql_file("custom_updates.sql")
+    except Exception as e: 
+        print("Error Occured: Please contact Vaibhav")
+        print(e)
+    try: 
+        repair_data()
     except Exception as e: 
         print("Error Occured: Please contact Vaibhav")
         print(e)
