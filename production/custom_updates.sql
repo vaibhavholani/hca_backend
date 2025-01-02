2d648be972b2ccc7cb54901f32b433ea3f6f9681
Vaibhav Holani <vaibhavholani@gmail.com>
2025-01-02 15:52:33 +0530

    intermediate update for custom_updates, the query for data migration is still wrong

--- production/custom_updates.sql
+++ production/custom_updates.sql
@@ -2,20 +2,23 @@ ALTER TABLE memo_bills
 ADD COLUMN bill_id INT;
 
 
-UPDATE memo_bills mb
-SET bill_id = sub.id
-FROM (
-    SELECT re.id, mb.id AS memo_bill_id
+WITH earliest_register_entries AS (
+    SELECT DISTINCT ON (re.supplier_id, re.party_id, re.bill_number) re.id AS register_id, 
+           mb.id AS memo_bill_id
     FROM memo_bills mb
     JOIN memo_entry me ON mb.memo_id = me.id
-    JOIN register_entry re ON re.supplier_id = me.supplier_id
-                           AND re.party_id = me.party_id
-                           AND re.bill_number = mb.bill_number
+    JOIN register_entry re
+      ON re.supplier_id = me.supplier_id
+      AND re.party_id = me.party_id
+      AND re.bill_number = mb.bill_number
     WHERE mb.bill_number != -1
-    ORDER BY re.register_date ASC
-    LIMIT 1
-) sub
-WHERE mb.id = sub.memo_bill_id;
+    ORDER BY re.supplier_id, re.party_id, re.bill_number, re.register_date
+)
+UPDATE memo_bills mb
+SET bill_id = e.register_id
+FROM earliest_register_entries e
+WHERE mb.id = e.memo_bill_id;
+
 
 
 SELECT COUNT(*) AS null_bill_ids
