1bfd5cb01493b588c02fcdbbb45c30d92dfdc71e
Vaibhav Holani <vaibhavholani@gmail.com>
2025-01-02 16:39:37 +0530

    correct data migration update

--- production/custom_updates.sql
+++ production/custom_updates.sql
@@ -1,16 +1,45 @@
 ALTER TABLE memo_bills
 ADD COLUMN bill_id INT;
 
+DO $$
+BEGIN
+    IF NOT EXISTS (
+        SELECT 1 
+        FROM information_schema.views 
+        WHERE table_name = 'unique_register_entries'
+    ) THEN
+        CREATE VIEW unique_register_entries AS
+        SELECT DISTINCT ON (supplier_id, party_id, bill_number) 
+               id, 
+               supplier_id, 
+               party_id, 
+               register_date, 
+               amount, 
+               bill_number, 
+               gr_amount, 
+               deduction, 
+               status, 
+               partial_amount, 
+               last_update
+        FROM register_entry
+        ORDER BY supplier_id, party_id, bill_number, register_date ASC;
+    END IF;
+END $$;
+
 
 WITH earliest_register_entries AS (
-    SELECT DISTINCT ON (re.supplier_id, re.party_id, re.bill_number) re.id AS register_id, 
+    SELECT
+           re.id AS register_id, 
+           re.bill_number AS bill_number,
+           re.register_date AS register_date,
            mb.id AS memo_bill_id
     FROM memo_bills mb
-    JOIN memo_entry me ON mb.memo_id = me.id
-    JOIN register_entry re
+    JOIN memo_entry me 
+      ON mb.memo_id = me.id
+    JOIN unique_register_entries re
       ON re.supplier_id = me.supplier_id
-      AND re.party_id = me.party_id
-      AND re.bill_number = mb.bill_number
+     AND re.party_id   = me.party_id
+     AND re.bill_number = mb.bill_number
     WHERE mb.bill_number != -1
     ORDER BY re.supplier_id, re.party_id, re.bill_number, re.register_date
 )
@@ -24,6 +53,8 @@ WHERE mb.id = e.memo_bill_id;
 SELECT COUNT(*) AS null_bill_ids
 FROM memo_bills
 WHERE bill_id IS NULL;
+
+DROP VIEW IF EXISTS unique_register_entries;
 
 ALTER TABLE memo_bills
 DROP COLUMN bill_number;
