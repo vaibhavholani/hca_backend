d47a230b6625b980ab10cd79228a01c3137fa6d8
Vaibhav Holani <vaibhavholani@gmail.com>
2025-01-02 15:51:40 +0530

    improving backup taking and backup restoration

--- backup/restore_backup.sh
+++ backup/restore_backup.sh
@@ -1,1 +1,11 @@
-psql -U postgres hca_backup < ~/development/backups/backup_28_Jul_2023.sql
+#!/bin/bash
+
+DB_USER="postgres"
+DB_NAME="hca_backup"
+BACKUP_FILE="/Users/vaibhavholani/development/business/holani_cloth_agency/backups/backup_24_Dec_2024.sql"
+
+psql -U "$DB_USER" -d postgres -c "DROP DATABASE IF EXISTS $DB_NAME;"
+psql -U "$DB_USER" -d postgres -c "CREATE DATABASE $DB_NAME;"
+psql -U "$DB_USER" "$DB_NAME" < "$BACKUP_FILE"
+
+echo "Database $DB_NAME restored successfully from $BACKUP_FILE."
