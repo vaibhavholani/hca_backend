0a375136b372aad5033b10756b20c50142249059
Vaibhav Holani <vaibhavholani@gmail.com>
2025-03-01 18:06:09 +0530

    server run file updates

--- production/mac_server/run.sh
+++ production/mac_server/run.sh
@@ -1,12 +1,17 @@
 #!/bin/bash
 
 # Run update command
-bash ./update.sh
+bash /Users/vaibhavholani/development/business/holani_cloth_agency/backend/production/mac_server/update.sh
 
 # >>> conda initialize >>>
 # !! Contents within this block are managed by 'conda init' !!
-source ~/miniconda3/etc/profile.d/conda.sh
+source  /Users/vaibhavholani/miniconda3/etc/profile.d/conda.sh
 # <<< conda initialize <<<
+
+export PATH=/Library/PostgreSQL/13/bin:$PATH
+
+# Stay awake even with closed lid
+caffeinate -dims &
 
 # Activate the conda environment located at the specified path
 conda activate /Users/vaibhavholani/development/business/hca_venv
